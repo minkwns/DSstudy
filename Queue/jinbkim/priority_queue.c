b4e243e9e0189cf537763d4bb9855ccc61236767
jinbkim <jinbkim@student.42seoul.kr>
2020-08-01 18:49:03 +0900

    20200801

--- Queue/jinbkim/priority_queue.c
+++ Queue/jinbkim/priority_queue.c
@@ -42,8 +42,65 @@ void	enter_p_queue(t_p_queue *pq, pq_data data)
 {
 	int	i;
 
+	i = ++(pq->data_num);
+	while (i != 1)
+	{
+		if (pq->f(pq->pq_arr[i / 2], data) < 0)
+			break ;
+		else
+		{
+			pq->pq_arr[i] = pq->pq_arr[i / 2];
+			i /= 2;
+		}
+	}
+	pq->pq_arr[i] = data;
 }
 
+int		p_queue_is_empty(t_p_queue *pq)
+{
+	if (pq->data_num)
+		return (0);
+	return (1);
+}
+
+int		get_high_priority_child_i(t_p_queue *pq, int i)
+{
+		if (pq->data_num < i * 2)
+			return (0);
+		else if (pq->data_num == i * 2)
+			return (i * 2);
+		else
+		{
+			if (pq->f(pq->pq_arr[i * 2], pq->pq_arr[i * 2 + 1]) < 0)
+				return (i * 2);
+			else
+				return (i * 2 + 1);
+		}
+		
+}
+
+pq_data	delete_p_queue(t_p_queue *pq)
+{
+	pq_data		remem_data;
+	pq_data		last_data;
+	int			parent_i;
+	int			child_i;
+
+	remem_data = pq->pq_arr[1];
+	last_data = pq->pq_arr[(pq->data_num)--];
+	parent_i = 1;
+	while ((child_i = get_high_priority_child_i(pq, parent_i)))
+	{
+		if (pq->f(last_data, pq->pq_arr[child_i]) >= 0)
+			break ;
+		pq->pq_arr[parent_i] = pq->pq_arr[child_i];
+		parent_i = child_i;
+	}
+	
+	pq->pq_arr[parent_i] = last_data;
+	return (remem_data);
+
+}
 
 
 
@@ -52,8 +109,13 @@ int		main(void)
 	t_p_queue pq;
 
 	p_queue_init(&pq, comp_func);
-	
-	enter_p_queue(&pq, "kim");
-	enter_p_queue(&pq, "jin");
-	enter_p_queue(&pq, "bum");
+
+	enter_p_queue(&pq, "ab");
+	enter_p_queue(&pq, "jk");
+	enter_p_queue(&pq, "cd");
+	enter_p_queue(&pq, "gh");
+	enter_p_queue(&pq, "ef");
+
+	while (!p_queue_is_empty(&pq))
+		printf("%s \n", delete_p_queue(&pq));
 }
